c4135a4ced6006f9c543567d0fb8f64a76c7a985
rounith <rounithdharavath@gmail.com>
2024-12-05 12:25:14 +0530

    loading precode before entering into route. Used This.oRouter.getRoute("next").attachMatched(this.herculis) function to load

--- FioriApp/webapp/controller/View2.controller.js
+++ FioriApp/webapp/controller/View2.controller.js
@@ -5,6 +5,10 @@ sap.ui.define(
         return Controller.extend("cloud4c.fioriApp.controller.View2",{
             onInit:function(){
                 this.oRouter=this.getOwnerComponent().getRouter()
+                this.oRouter.getRoute("next").attachMatched(this.herculis)
+            },
+            herculis:function(){
+                console.log("Hello this is route name revanth")
             },
             onBack: function(){
                 //var oApp=this.getView().getParent()
