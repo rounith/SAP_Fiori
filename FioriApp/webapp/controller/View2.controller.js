dc065599b9e181ec6be86fd257cdeb05cab2d8a7
rounith <rounithdharavath@gmail.com>
2024-12-16 11:10:19 +0530

    F4 and popup: View2 an controller2

--- FioriApp/webapp/controller/View2.controller.js
+++ FioriApp/webapp/controller/View2.controller.js
@@ -1,8 +1,8 @@
 sap.ui.define(
     ["sap/ui/core/mvc/Controller",
-        "cloud4c/fioriApp/util/formatter"
+        "cloud4c/fioriApp/util/formatter",'sap/m/MessageToast'
     ],
-    function(Controller,Formatter){
+    function(Controller,Formatter,MessageToast){
         "use strict"
         return Controller.extend("cloud4c.fioriApp.controller.View2",{
             formatter: Formatter,
@@ -10,6 +10,10 @@ sap.ui.define(
                 this.oRouter=this.getOwnerComponent().getRouter()
                 this.oRouter.getRoute("next").attachMatched(this.herculis, this)
             },
+            showPopup:function(){
+                var msg = 'F4 working';
+                MessageToast.show(msg);
+            },  
             herculis:function(oEvent){
                 console.log("Hello this is route name revanth")
                 var myVar=  oEvent.getParameter("arguments").kaven
